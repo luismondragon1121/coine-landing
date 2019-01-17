ee571ee69a792313a879ef77a3ea5f5cb5666170
unknown <luismondragon1121>
2019-01-17 13:04:02 -0600

    se modifico conforme a cambio que se solicitaron

--- js/acciones.js
+++ js/acciones.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $('ul.tabs li a:first').addClass('active');
+    $('ul.tabs li a:first').addClass('active1');
     $('.inversion-secciones article').hide();
     $('.inversion-secciones article:first').show();
     
     $('ul.tabs li a').click(function(){
-        $('ul.tabs li a').removeClass('active');
-        $(this).addClass('active');
+        $('ul.tabs li a').removeClass('active1');
+        $(this).addClass('active1');
         $('.inversion-secciones article').hide();
         
         var activeTab =$(this).attr('href');
